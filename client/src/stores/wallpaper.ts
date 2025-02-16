6a79610196e022ece5150b7e2ed3636c5576ad18
Kan-A-Pesh <michelm.gaming@gmail.com>
2025-02-16 15:59:23 +0100

    feat: make wallpaper persistant

--- client/src/stores/wallpaper.ts
+++ client/src/stores/wallpaper.ts
@@ -1,16 +1,21 @@
 import {WallpaperKeys} from "@/components/xp/wallpaper/list";
 import {create} from "zustand";
+import {persist} from "zustand/middleware";
 
 interface WallpaperStore {
   wallpaper: WallpaperKeys;
   change: (wallpaper: WallpaperKeys) => void;
 }
 
-const useWallpaperStore = create<WallpaperStore>((set) => ({
-  wallpaper: "bliss",
-  change: (wallpaper: WallpaperKeys): void => {
-    set({wallpaper: wallpaper});
-  }
-}));
-
+const useWallpaperStore = create(
+  persist<WallpaperStore>(
+    (set) => ({
+      wallpaper: "bliss",
+      change: (wallpaper: WallpaperKeys): void => {
+        set({wallpaper: wallpaper});
+      }
+    }),
+    {name: "wallpaper-store"}
+  )
+);
 export default useWallpaperStore;
